0966ed868426797b66b62e9c137e410028c09662
KrySt <stejskalkryshi@gmail.com>
2017-10-18 16:54:55 +0200

    Add files via upload

---
+++ Obrázky.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 15:58:48 2017
+
+@author: kryshi
+"""
+
+x = ' '
+
+print("****\n")
+print("***\n")
+print("**\n")
+print("*\n")
+print()
+print(x*4)
+print("*\n")
+print(x*3)
+print("***\n")
+print(x*2)
+print("*****\n")
+print(x*1)
+print("*******\n")
+print("*********\n")
+print()
+print("*\n")
+print("**\n")
+print("***\n")
+print("****\n")
+print("***\n")
+print("**\n")
+print("*\n")
